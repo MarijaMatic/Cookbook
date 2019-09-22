f3c189cce12473c75e9fb5a38e4a94d027ce8c9b
Marija Matic <mrj.ivancic@gmail.com>
2019-09-22 13:11:06 +0200

    application properties & fixed classes

--- cookbook-backend/src/main/java/com/cookbookbackend/cookbookbackend/models/Recipe.java
+++ cookbook-backend/src/main/java/com/cookbookbackend/cookbookbackend/models/Recipe.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.List;
 
 @AllArgsConstructor
@@ -22,6 +19,7 @@ public class Recipe {
 
     private String name;
     private String description;
+    @ElementCollection
     private List<String> ingredientsList;
     private String steps;
 }
