26c56463155185c5ef3cb0c42e4af7a372eeecc6
Marija Matic <mrj.ivancic@gmail.com>
2019-09-22 16:30:50 +0200

    added mappings

--- cookbook-backend/src/main/java/com/cookbookbackend/cookbookbackend/controllers/CookbookController.java
+++ cookbook-backend/src/main/java/com/cookbookbackend/cookbookbackend/controllers/CookbookController.java
@@ -1,11 +1,12 @@
 package com.cookbookbackend.cookbookbackend.controllers;
 
 import com.cookbookbackend.cookbookbackend.models.Cookbook;
+import com.cookbookbackend.cookbookbackend.models.Recipe;
 import com.cookbookbackend.cookbookbackend.repositories.CookbookRepository;
+import com.cookbookbackend.cookbookbackend.repositories.RecipeRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,14 +14,31 @@ import java.util.List;
 public class CookbookController {
 
     private CookbookRepository cookbookRepo;
+    private RecipeRepository recipeRepo;
 
-    public CookbookController(CookbookRepository cookbookRepository){
+    public CookbookController(CookbookRepository cookbookRepository, RecipeRepository recipeRepository) {
         cookbookRepo = cookbookRepository;
+        recipeRepo = recipeRepository;
     }
 
     @GetMapping(value = "/allCookbooks")
-    public ResponseEntity<List<Cookbook>> findAllCookbooks (){
+    public ResponseEntity<List<Cookbook>> findAllCookbooks() {
         List<Cookbook> allCookbooks = cookbookRepo.findAll();
         return new ResponseEntity<>(allCookbooks, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/newCookbook", consumes = "application/json")
+    public ResponseEntity<Cookbook> writeCookbook(@RequestBody Cookbook newCookbook) {
+        cookbookRepo.save(newCookbook);
+        return new ResponseEntity<>(newCookbook, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/cookbooks/{cookbookID}/addRecipe/{recipeID}")
+    public ResponseEntity<Cookbook> addRecipeToCookbook(@PathVariable Long recipeID, @PathVariable Long cookbookID) {
+        Cookbook selectedCookbook = cookbookRepo.findById(cookbookID).get();
+        Recipe selectedRecipe = recipeRepo.findById(recipeID).get();
+        selectedCookbook.getRecipes().add(selectedRecipe);
+        cookbookRepo.save(selectedCookbook);
+        return new ResponseEntity<>(selectedCookbook, HttpStatus.OK);
     }
 }
--- cookbook-backend/src/main/java/com/cookbookbackend/cookbookbackend/controllers/RecipeController.java
+++ cookbook-backend/src/main/java/com/cookbookbackend/cookbookbackend/controllers/RecipeController.java
@@ -5,6 +5,8 @@ import com.cookbookbackend.cookbookbackend.repositories.RecipeRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,5 +24,11 @@ public class RecipeController {
     public ResponseEntity<List<Recipe>> getAllRecipes(){
         List<Recipe> allRecipes = recipeRepo.findAll();
         return new ResponseEntity<>(allRecipes, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/addRecipe", consumes = "application/json")
+    public ResponseEntity<Recipe> writeRecipe(@RequestBody Recipe newRecipe){
+        recipeRepo.save(newRecipe);
+        return new ResponseEntity<>(newRecipe, HttpStatus.OK);
     }
 }
