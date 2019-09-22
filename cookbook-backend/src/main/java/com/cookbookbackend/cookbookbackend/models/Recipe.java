5c1da985a807e9738f53a1098fd586844d5f2be1
Marija Matic <mrj.ivancic@gmail.com>
2019-09-22 11:55:05 +0200

    added the classes

---
+++ cookbook-backend/src/main/java/com/cookbookbackend/cookbookbackend/models/Recipe.java
@@ -0,0 +1,27 @@
+package com.cookbookbackend.cookbookbackend.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String description;
+    private List<String> ingredientsList;
+    private String steps;
+}
